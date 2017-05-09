c6c0b7244401f680a944045b53d60975b5b82a3f
huarui <huarui.wu@gmail.com>
2017-05-09 15:48:43 -0700

    try passing cookie

--- src/util/ajax.js
+++ src/util/ajax.js
@@ -33,11 +33,10 @@ exports.getJSON = function(url, callback) {
     return xhr;
 };
 
-exports.getArrayBuffer = function(url, callback, withCredentials) {
+exports.getArrayBuffer = function(url, callback, cookie) {
     const xhr = new window.XMLHttpRequest();
     xhr.open('GET', url, true);
-    // xhr.withCredentials = withCredentials;
-    xhr.setRequestHeader('cookie', document.cookie)
+    xhr.setRequestHeader('cookie', cookie)
     xhr.responseType = 'arraybuffer';
     xhr.onerror = function(e) {
         callback(e);
@@ -68,7 +67,7 @@ function sameOrigin(url) {
 
 const transparentPngUrl = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQYV2NgAAIAAAUAAarVyFEAAAAASUVORK5CYII=';
 
-exports.getImage = function(url, callback, withCredentials) {
+exports.getImage = function(url, callback, cookie) {
     // request the image with XHR to work around caching issues
     // see https://github.com/mapbox/mapbox-gl-js/issues/1470
     return exports.getArrayBuffer(url, (err, imgData) => {
@@ -83,7 +82,7 @@ exports.getImage = function(url, callback, withCredentials) {
         img.cacheControl = imgData.cacheControl;
         img.expires = imgData.expires;
         img.src = imgData.data.byteLength ? URL.createObjectURL(blob) : transparentPngUrl;
-    }, withCredentials);
+    }, cookie);
 };
 
 exports.getVideo = function(urls, callback) {
